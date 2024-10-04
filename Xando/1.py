2c0b3380e5d7c276b8ef3412398714c06dd30fd3
pixo2000 <firecraftpixo@gmail.com>
2024-10-04 14:00:37 +0200

    just some shit

---
+++ Xando/1.py
@@ -0,0 +1,12 @@
+tag = 4
+monat = 10
+jahr = 2024
+# 2, 7 und 8
+def calcal():
+    if monat = 2 or 7 or 8:
+        if monat = 2:
+            monthdays = 28
+        if monat = 7 or 8:
+            monthdays = 31
+    else: 
+    
