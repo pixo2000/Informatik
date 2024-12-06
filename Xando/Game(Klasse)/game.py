01dd36f0bdfcf529c0ee27bdf6e641b05fadb7e8
pixo2000 <firecraftpixo@gmail.com>
2024-12-06 13:19:08 +0100

    added the entire game stuff. gonna add movement now

---
+++ Xando/Game(Klasse)/game.py
@@ -0,0 +1,31 @@
+from logging_functions import log
+
+def print_game(game):
+    log.info("Printing game")
+    for line in game:
+        print(line)
+
+def create_game_area(height, width):
+    log.info("Creating game area with height: %s, width: %s", height, width)
+    result = []
+
+    for line in range(height):
+        result += ["0" * width]
+
+    return result
+
+def read_coords(game ,x, y):
+    log.info("Reading coordinates x: %s, y: %s", x, y)
+    line = game[y]
+
+    return line[x]
+
+def write_coords(game, x, y, symbol):
+    log.info("Writing symbol %s to coordinates x: %s, y: %s", symbol, x, y)
+    result = game.copy()
+
+    line = result[y]
+
+    result[y] = line[:x] + symbol + line[x + 1:]
+
+    return result
--- Xando/Game(Klasse)/main.py
+++ Xando/Game(Klasse)/main.py
@@ -1,7 +1,16 @@
-def zeichneSterne(n):
-    while n > 0:
-        print ("*", end="")
-        n = n - 1
-    print("")
+import game
+import logging_functions as logger
+from logging_functions import log
 
-zeichneSterne(15)
+
+def run_game():
+    runningGame = game.create_game_area(10, 5)
+    runningGame = game.write_coords(runningGame, 3, 3, "X")
+    game.print_game(runningGame)
+def main():
+    logger.start_logging("DEBUG")
+    run_game()
+    log.info("Game finished")
+
+if __name__ == '__main__':
+    main()
