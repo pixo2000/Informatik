211d08d939f79d64246a77e4c8031aa114118bd0
pixo2000 <firecraftpixo@gmail.com>
2024-12-06 13:51:17 +0100

    added movement but got bug and needs refactor

--- Xando/Game(Klasse)/main.py
+++ Xando/Game(Klasse)/main.py
@@ -4,9 +4,13 @@ from logging_functions import log
 
 
 def run_game():
+    # set startcoords here
+    startcoords = (3, 3)
     runningGame = game.create_game_area(10, 5)
-    runningGame = game.write_coords(runningGame, 3, 3, "X")
+    runningGame = game.write_coords(runningGame, startcoords[0], startcoords[1], "X")
     game.print_game(runningGame)
+    game.control(runningGame)
+
 def main():
     logger.start_logging("DEBUG")
     run_game()
