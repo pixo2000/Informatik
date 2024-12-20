dc7f7b6d3b04d59af724d1f1bffceec0f2b2da0c
Xandarian <FireCraftpixo@gmail.com>
2024-12-20 21:48:14 +0100

    moved files, implemented placing of ores

--- Xando/Game(Klasse)/main.py
+++ Xando/Game(Klasse)/main.py
@@ -1,5 +1,6 @@
 import Spielfeld
 import Spielfigur
+from Gegenstaende import platziere_gold, platziere_silber, platziere_platin
 
 
 def run_game():
@@ -9,6 +10,11 @@ def run_game():
     mySpielfeld = Spielfeld.erzeuge(10, 10)
     mySpielfeld = Spielfeld.schreibe(mySpielfeld, x, y, "X")
     Spielfeld.ausgabe(mySpielfeld)
+
+    # Place items on the game field
+    mySpielfeld = platziere_gold(mySpielfeld, 5)
+    mySpielfeld = platziere_silber(mySpielfeld, 3)
+    mySpielfeld = platziere_platin(mySpielfeld, 2)
 
     while True:
         (mySpielfeld, x, y) = Spielfigur.bewege(mySpielfeld, x, y)
